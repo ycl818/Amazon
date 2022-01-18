c55ce20ce99c6ba5d0b81388f285e1b49247c63d
ycl818 <kevinyichienlee@gmail.com>
2022-01-18 00:07:08 -0500

    ADD: cart total HTML/style

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -4,7 +4,14 @@ import './CartTotal.css'
 function CartTotal() {
   return (
     <div className='CartTotal'>
-      
+      <h3>Subtotal(5 items) :
+          <span className='CartTotal-price'>
+            $2,172.38
+          </span>
+      </h3>
+      <button>
+        Proceed to checkout
+      </button>
     </div>
   )
 }

91372c9b2da7baadfad4fba1fd0088565ee598d5
ycl818 <kevinyichienlee@gmail.com>
2022-01-18 00:21:08 -0500

    ADD:calc total price

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
      <Header title="Amazon Cart" />
       <div className='App-main'>
         <CartItems items={cartItems} />
-        <CartTotal />
+        <CartTotal  items={cartItems}/>
       </div>
     </div>
   )
--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './CartTotal.css'
 
-function CartTotal() {
+function CartTotal({ items }) {
+
+  const getTotalPrice = () => {
+    let total = 0;
+    items.forEach((item)=> {
+      total += (item.price * item.quantity)
+    })
+    return total;
+  }
+
   return (
     <div className='CartTotal'>
-      <h3>Subtotal(5 items) :
+      <h3>Subtotal({items.length} items) :
           <span className='CartTotal-price'>
-            $2,172.38
+            ${getTotalPrice()}
           </span>
       </h3>
       <button>
